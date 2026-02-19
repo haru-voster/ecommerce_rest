afd7de15c5348cdf0d14de2850edffeb425cdd1a
haru-voster <wabwireharoun2001@gmail.com>
2026-02-19 16:15:51 +0300

    settings

--- ruestore/urls.py
+++ ruestore/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductList
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
+    path("products/", ProductList.as_view()),
 ]
