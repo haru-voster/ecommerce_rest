a34cbc06dc907f471a44cbb6ed32ec51c1ccf721
haru-voster <wabwireharoun2001@gmail.com>
2026-02-19 15:48:33 +0300

    models

--- ruestore/models.py
+++ ruestore/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    price = models.IntegerField()
+    old_price = models.IntegerField(null=True, blank=True)
+    ram = models.CharField(max_length=50)
+    storage = models.CharField(max_length=50)
+    processor = models.CharField(max_length=100)
+    display = models.CharField(max_length=100)
+    graphics = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products/')
+    def __str__(self):
+        return self.name
