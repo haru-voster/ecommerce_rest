7f6ba07c03cc84b76380dbe17db630f9331fbc53
haru-voster <wabwireharoun2001@gmail.com>
2026-02-21 23:08:40 +0300

    static

--- ruestore/urls.py
+++ ruestore/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 import ruestore.views as views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
    path("", views.home, name="home"),
    path('products/', views.ProductList.as_view(), name='product-list'),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
