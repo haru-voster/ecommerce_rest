bf0395157dbc717fe8151a7674c145eb3eae8ddf
haru-voster <wabwireharoun2001@gmail.com>
2026-02-23 14:45:01 +0300

    products- js

--- comps/settings.py
+++ comps/settings.py
@@ -125,6 +125,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
--- ruestore/serializers.py
+++ ruestore/serializers.py
@@ -1,15 +1,26 @@
-
 from rest_framework import serializers
 from .models import Product, ProductImage
 
+
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductImage
-        fields = ['image']
+        fields = ["id", "image"]
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.image:
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+        return None
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)  # fetch all images
+    images = ProductImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
-        fields = ['id','name','price','ram','processor','storage','graphics','display','camera','warranty','description','images']
+        fields = "__all__"
