0e192e44344beab24fb787210382b5567cb7dc26
haru-voster <wabwireharoun2001@gmail.com>
2026-03-03 08:57:58 +0300

    paging settings

--- comps/settings.py
+++ comps/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# added paging 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4,
+}
